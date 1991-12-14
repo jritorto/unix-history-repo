d60c6e791f13efd8247e44f6109fa298fd0b66e3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-12-13 20:09:32 -0800

    fix lockf code; have getcanonname recognize canonical names
    SCCS-vsn: 5.47

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	5.46 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	5.47 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
