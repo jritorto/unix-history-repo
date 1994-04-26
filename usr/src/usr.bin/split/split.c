a348e5a5283022b698d5556cf1c4fb34f0c19589
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-25 18:39:10 -0800

    typo (NULL pointer dereference)
    SCCS-vsn: 8.3

--- usr/src/usr.bin/split/split.c
+++ usr/src/usr.bin/split/split.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)split.c	8.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)split.c	8.3 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
