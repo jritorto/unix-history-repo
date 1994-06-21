7fc0f45d9bf88a8cf67e46744c973f37f5772968
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-06-20 20:34:53 -0800

    byte swapping code for LITTLE_ENDIAN on old format tapes (from mycroft)
    SCCS-vsn: 8.3

--- usr/src/sbin/restore/dirs.c
+++ usr/src/sbin/restore/dirs.c
@@ -11,7 +11,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)dirs.c	8.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)dirs.c	8.3 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
