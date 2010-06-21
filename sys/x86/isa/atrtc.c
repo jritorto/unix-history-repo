jritorto/unix-history-repo

commit c48e2a89b28cc1c8f1c705f46d76673def890c32

Implement new event timers infrastructure. It provides unified APIs for writing event timer drivers, for choosing best possible drivers by machine independent code and for operating them to supply kernel with hardclock(), statclock() and profclock() events in unified fashion on various hardware.
Infrastructure provides support for both per-CPU (independent for every CPU
core) and global timers in periodic and one-shot modes. MI management code
at this moment uses only periodic mode, but one-shot mode use planned for
later, as part of tickless kernel project.

For this moment infrastructure used on i386 and amd64 architectures. Other
archs are welcome to follow, while their current operation should not be
affected.

This patch updates existing drivers (i8254, RTC and LAPIC) for the new
order, and adds event timers support into the HPET driver. These drivers
have different capabilities:
 LAPIC - per-CPU timer, supports periodic and one-shot operation, may
freeze in C3 state, calibrated on first use, so may be not exactly precise.
 HPET - depending on hardware can work as per-CPU or global, supports
periodic and one-shot operation, usually provides several event timers.
 i8254 - global, limited to periodic mode, because same hardware used also
as time counter.
 RTC - global, supports only periodic mode, set of frequencies in Hz
limited by powers of 2.

Depending on hardware capabilities, drivers preferred in following orders,
either LAPIC, HPETs, i8254, RTC or HPETs, LAPIC, i8254, RTC.
User may explicitly specify wanted timers via loader tunables or sysctls:
kern.eventtimer.timer1 and kern.eventtimer.timer2.
If requested driver is unavailable or unoperational, system will try to
replace it. If no more timers available or "NONE" specified for second,
system will operate using only one timer, multiplying it's frequency by few
times and uing respective dividers to honor hz, stathz and profhz values,
set during initial setup.

sys/x86/isa/atrtc.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2008 Poul-Henning Kamp
+ * Copyright (c) 2010 Alexander Motin <mav@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -39,17 +40,20 @@ __FBSDID("$FreeBSD$");
 #include <sys/mutex.h>
 #include <sys/kernel.h>
 #include <sys/module.h>
+#include <sys/proc.h>
+#include <sys/rman.h>
+#include <sys/timeet.h>
 
 #include <isa/rtc.h>
 #ifdef DEV_ISA
 #include <isa/isareg.h>
 #include <isa/isavar.h>
 #endif
+#include <machine/intr_machdep.h>
+#include "clock_if.h"
 
 #define	RTC_LOCK	mtx_lock_spin(&clock_lock)
 #define	RTC_UNLOCK	mtx_unlock_spin(&clock_lock)
-
-int	atrtcclock_disable = 0;
 
 static	int	rtc_reg = -1;
 static	u_char	rtc_statusa = RTCSA_DIVIDER | RTCSA_NOPROF;
@@ -98,7 +102,7 @@ readrtc(int port)
 	return(bcd2bin(rtcin(port)));
 }
 
-void
+static void
 atrtc_start(void)
 {
 
@@ -106,7 +110,7 @@ atrtc_start(void)
 	writertc(RTC_STATUSB, RTCSB_24HR);
 }
 
-void
+static void
 atrtc_rate(unsigned rate)
 {
 
@@ -114,11 +118,20 @@ atrtc_rate(unsigned rate)
 	writertc(RTC_STATUSA, rtc_statusa);
 }
 
-void
+static void
 atrtc_enable_intr(void)
 {
 
 	rtc_statusb |= RTCSB_PINTR;
+	writertc(RTC_STATUSB, rtc_statusb);
+	rtcin(RTC_INTR);
+}
+
+static void
+atrtc_disable_intr(void)
+{
+
+	rtc_statusb &= ~RTCSB_PINTR;
 	writertc(RTC_STATUSB, rtc_statusb);
 	rtcin(RTC_INTR);
 }
@@ -135,40 +148,73 @@ atrtc_restore(void)
 	rtcin(RTC_INTR);
 }
 
-int
-atrtc_setup_clock(void)
-{
-	int diag;
-
-	if (atrtcclock_disable)
-		return (0);
-
-	diag = rtcin(RTC_DIAG);
-	if (diag != 0) {
-		printf("RTC BIOS diagnostic error %b\n",
-		    diag, RTCDG_BITS);
-		return (0);
-	}
-
-	stathz = RTC_NOPROFRATE;
-	profhz = RTC_PROFRATE;
-
-	return (1);
-}
-
 /**********************************************************************
  * RTC driver for subr_rtc
  */
-
-#include "clock_if.h"
-
-#include <sys/rman.h>
 
 struct atrtc_softc {
 	int port_rid, intr_rid;
 	struct resource *port_res;
 	struct resource *intr_res;
+	void *intr_handler;
+	struct eventtimer et;
 };
+
+static int
+rtc_start(struct eventtimer *et,
+    struct bintime *first, struct bintime *period)
+{
+
+	atrtc_rate(max(fls((period->frac + (period->frac >> 1)) >> 32) - 17, 1));
+	atrtc_enable_intr();
+	return (0);
+}
+
+static int
+rtc_stop(struct eventtimer *et)
+{
+
+	atrtc_disable_intr();
+	return (0);
+}
+
+/*
+ * This routine receives statistical clock interrupts from the RTC.
+ * As explained above, these occur at 128 interrupts per second.
+ * When profiling, we receive interrupts at a rate of 1024 Hz.
+ *
+ * This does not actually add as much overhead as it sounds, because
+ * when the statistical clock is active, the hardclock driver no longer
+ * needs to keep (inaccurate) statistics on its own.  This decouples
+ * statistics gathering from scheduling interrupts.
+ *
+ * The RTC chip requires that we read status register C (RTC_INTR)
+ * to acknowledge an interrupt, before it will generate the next one.
+ * Under high interrupt load, rtcintr() can be indefinitely delayed and
+ * the clock can tick immediately after the read from RTC_INTR.  In this
+ * case, the mc146818A interrupt signal will not drop for long enough
+ * to register with the 8259 PIC.  If an interrupt is missed, the stat
+ * clock will halt, considerably degrading system performance.  This is
+ * why we use 'while' rather than a more straightforward 'if' below.
+ * Stat clock ticks can still be lost, causing minor loss of accuracy
+ * in the statistics, but the stat clock will no longer stop.
+ */
+static int
+rtc_intr(void *arg)
+{
+	struct atrtc_softc *sc = (struct atrtc_softc *)arg;
+	int flag = 0;
+
+	while (rtcin(RTC_INTR) & RTCIR_PERIOD) {
+		flag = 1;
+		if (sc->et.et_active) {
+			sc->et.et_event_cb(&sc->et,
+			    sc->et.et_arg ? sc->et.et_arg :
+			    curthread->td_intr_frame);
+		}
+	}
+	return(flag ? FILTER_HANDLED : FILTER_STRAY);
+}
 
 /*
  * Attach to the ISA PnP descriptors for the timer and realtime clock.
@@ -196,23 +242,42 @@ static int
 atrtc_attach(device_t dev)
 {
 	struct atrtc_softc *sc;
-	int i;
+	int i, diag, haveirq = 0;
 
-	/*
-	 * Not that we need them or anything, but grab our resources
-	 * so they show up, correctly attributed, in the big picture.
-	 */
-	
 	sc = device_get_softc(dev);
 	if (!(sc->port_res = bus_alloc_resource(dev, SYS_RES_IOPORT,
 	    &sc->port_rid, IO_RTC, IO_RTC + 1, 2, RF_ACTIVE)))
 		device_printf(dev,"Warning: Couldn't map I/O.\n");
 	if (!(sc->intr_res = bus_alloc_resource(dev, SYS_RES_IRQ,
 	    &sc->intr_rid, 8, 8, 1, RF_ACTIVE)))
-		device_printf(dev,"Warning: Couldn't map Interrupt.\n");
+		device_printf(dev,"Couldn't map Interrupt.\n");
+	else if ((bus_setup_intr(dev, sc->intr_res,
+	    INTR_MPSAFE | INTR_TYPE_CLK, (driver_filter_t *)rtc_intr, NULL,
+	    sc, &sc->intr_handler))) {
+		device_printf(dev, "Can't setup interrupt.\n");
+	} else { 
+		haveirq = 1;
+		/* Bind IRQ to BSP to avoid live migration. */
+		bus_bind_intr(dev, sc->intr_res, 0);
+	}
+	diag = rtcin(RTC_DIAG);
+	if (diag != 0)
+		printf("RTC BIOS diagnostic error %b\n", diag, RTCDG_BITS);
+	atrtc_start();
 	clock_register(dev, 1000000);
-	if (resource_int_value("atrtc", 0, "clock", &i) == 0 && i == 0)
-		atrtcclock_disable = 1;
+	bzero(&sc->et, sizeof(struct eventtimer));
+	if (haveirq &&
+	    (resource_int_value(device_get_name(dev), device_get_unit(dev),
+	     "clock", &i) != 0 || i != 0)) {
+		sc->et.et_name = "RTC";
+		sc->et.et_flags = ET_FLAGS_PERIODIC;
+		sc->et.et_quality = 0;
+		sc->et.et_frequency = 32768;
+		sc->et.et_start = rtc_start;
+		sc->et.et_stop = rtc_stop;
+		sc->et.et_priv = dev;
+		et_register(&sc->et);
+	}
 	return(0);
 }
 

sys/amd64/amd64/mp_machdep.c
@@ -706,10 +706,11 @@ init_secondary(void)
 	load_es(_udatasel);
 	load_fs(_ufssel);
 	mtx_unlock_spin(&ap_boot_mtx);
-
 	/* wait until all the AP's are up */
 	while (smp_started == 0)
 		ia32_pause();
+	/* Start per-CPU event timers. */
+	cpu_initclocks_ap();
 
 	sched_throw(NULL);
 

sys/amd64/include/apicvar.h
@@ -158,12 +158,6 @@
 #define	APIC_BUS_PCI		2
 #define	APIC_BUS_MAX		APIC_BUS_PCI
 
-enum lapic_clock {
-	LAPIC_CLOCK_NONE,
-	LAPIC_CLOCK_HARDCLOCK,
-	LAPIC_CLOCK_ALL
-};
-
 /*
  * An APIC enumerator is a psuedo bus driver that enumerates APIC's including
  * CPU's and I/O APIC's.
@@ -234,7 +228,6 @@ int	lapic_set_lvt_triggermode(u_int apic_id, u_int lvt,
 	    enum intr_trigger trigger);
 void	lapic_set_tpr(u_int vector);
 void	lapic_setup(int boot);
-enum lapic_clock	lapic_setup_clock(enum lapic_clock srcsdes);
 
 #endif /* !LOCORE */
 #endif /* _MACHINE_APICVAR_H_ */

sys/conf/files
@@ -2085,6 +2085,7 @@ kern/kern_context.c		standard
 kern/kern_descrip.c		standard
 kern/kern_dtrace.c		optional kdtrace_hooks
 kern/kern_environment.c		standard
+kern/kern_et.c			standard
 kern/kern_event.c		standard
 kern/kern_exec.c		standard
 kern/kern_exit.c		standard

sys/conf/files.amd64
@@ -297,6 +297,7 @@ x86/cpufreq/p4tcc.c		optional	cpufreq
 x86/isa/atpic.c			optional	atpic isa
 x86/isa/atrtc.c			standard
 x86/isa/clock.c			standard
+x86/x86/timeevents.c		standard
 x86/isa/elcr.c			standard
 x86/isa/isa.c			standard
 x86/isa/isa_dma.c		standard

sys/conf/files.i386
@@ -381,6 +381,7 @@ x86/cpufreq/smist.c		optional cpufreq
 x86/isa/atpic.c			optional atpic	
 x86/isa/atrtc.c			optional atpic
 x86/isa/clock.c			optional native
+x86/x86/timeevents.c		standard
 x86/isa/elcr.c			standard
 x86/isa/isa.c			optional isa
 x86/isa/isa_dma.c		optional isa

commit 164a51d54ea5889dd28d6a9bcc99f8e9592aac85

Fix i386 LINT build broken by r209371. There appeared such legacy thing as APM, that somehow breaking RTC.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@209401 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/x86/isa/atrtc.c
@@ -55,6 +55,8 @@ __FBSDID("$FreeBSD$");
 #define	RTC_LOCK	mtx_lock_spin(&clock_lock)
 #define	RTC_UNLOCK	mtx_unlock_spin(&clock_lock)
 
+int	atrtcclock_disable = 0;
+
 static	int	rtc_reg = -1;
 static	u_char	rtc_statusa = RTCSA_DIVIDER | RTCSA_NOPROF;
 static	u_char	rtc_statusb = RTCSB_24HR;
@@ -267,6 +269,7 @@ atrtc_attach(device_t dev)
 	clock_register(dev, 1000000);
 	bzero(&sc->et, sizeof(struct eventtimer));
 	if (haveirq &&
+	    !atrtcclock_disable &&
 	    (resource_int_value(device_get_name(dev), device_get_unit(dev),
 	     "clock", &i) != 0 || i != 0)) {
 		sc->et.et_name = "RTC";