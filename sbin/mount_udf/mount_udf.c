jritorto/unix-history-repo

commit 1f6ab012343185e18107f85efd5aa6cfbcb50f5f

Use build_iovec() to make it less cryptic.  This also fixes warnings.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@247861 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sbin/mount_udf/mount_udf.c
@@ -73,14 +73,15 @@ void	usage(void);
 int
 main(int argc, char **argv)
 {
-	struct iovec iov[12];
-	int ch, i, mntflags, udf_flags;
-	char *dev, *dir, mntpath[MAXPATHLEN];
-	char *cs_disk, *cs_local;
-	int verbose;
+	char mntpath[MAXPATHLEN];
+	char fstype[] = "udf";
+	struct iovec *iov;
+	char *cs_disk, *cs_local, *dev, *dir;
+	int ch, i, iovlen, mntflags, udf_flags, verbose;
 
-	i = mntflags = udf_flags = verbose = 0;
+	i = iovlen = mntflags = udf_flags = verbose = 0;
 	cs_disk = cs_local = NULL;
+	iov = NULL;
 	while ((ch = getopt(argc, argv, "o:vC:")) != -1)
 		switch (ch) {
 		case 'o':
@@ -120,32 +121,13 @@ main(int argc, char **argv)
 	 */
 	mntflags |= MNT_RDONLY;
 
-	iov[i].iov_base = "fstype";
-	iov[i++].iov_len = sizeof("fstype");
-	iov[i].iov_base = "udf";
-	iov[i].iov_len = strlen(iov[i].iov_base) + 1;
-	i++;
-	iov[i].iov_base = "fspath";
-	iov[i++].iov_len = sizeof("fspath");
-	iov[i].iov_base = mntpath;
-	iov[i++].iov_len = strlen(mntpath) + 1;
-	iov[i].iov_base = "from";
-	iov[i++].iov_len = sizeof("from");
-	iov[i].iov_base = dev;
-	iov[i++].iov_len = strlen(dev) + 1;
-	iov[i].iov_base = "flags";
-	iov[i++].iov_len = sizeof("flags");
-	iov[i].iov_base = &udf_flags;
-	iov[i++].iov_len = sizeof(udf_flags);
+	build_iovec(&iov, &iovlen, "fstype", fstype, (size_t)-1);
+	build_iovec(&iov, &iovlen, "fspath", mntpath, (size_t)-1);
+	build_iovec(&iov, &iovlen, "from", dev, (size_t)-1);
+	build_iovec(&iov, &iovlen, "flags", &udf_flags, sizeof(udf_flags));
 	if (udf_flags & UDFMNT_KICONV) {
-		iov[i].iov_base = "cs_disk";
-		iov[i++].iov_len = sizeof("cs_disk");
-		iov[i].iov_base = cs_disk;
-		iov[i++].iov_len = strlen(cs_disk) + 1;
-		iov[i].iov_base = "cs_local";
-		iov[i++].iov_len = sizeof("cs_local");
-		iov[i].iov_base = cs_local;
-		iov[i++].iov_len = strlen(cs_local) + 1;
+		build_iovec(&iov, &iovlen, "cs_disk", cs_disk, (size_t)-1);
+		build_iovec(&iov, &iovlen, "cs_local", cs_local, (size_t)-1);
 	}
 	if (nmount(iov, i, mntflags) < 0)
 		err(1, "%s", dev);

sbin/mount_udf/Makefile
@@ -9,7 +9,6 @@ LDADD=	-lkiconv
 MOUNT=	${.CURDIR}/../mount
 CFLAGS+= -I${MOUNT} -I${.CURDIR}/../../sys
 .PATH:	${MOUNT}
-WARNS?= 1
 
 # Needs to be dynamically linked for optional dlopen() access to
 # userland libiconv

commit 04950b53818beb7356b81bd71fb1ee6e5306085c

MFC: r261496
Unbreak mount_udf by passing the correct iovec length into
nmount().  This has been broken since r247861.

PR:		bin/186193
Submitted by:	Arnot Belohlavek

sbin/mount_udf/mount_udf.c
@@ -77,9 +77,9 @@ main(int argc, char **argv)
 	char fstype[] = "udf";
 	struct iovec *iov;
 	char *cs_disk, *cs_local, *dev, *dir;
-	int ch, i, iovlen, mntflags, udf_flags, verbose;
+	int ch, iovlen, mntflags, udf_flags, verbose;
 
-	i = iovlen = mntflags = udf_flags = verbose = 0;
+	iovlen = mntflags = udf_flags = verbose = 0;
 	cs_disk = cs_local = NULL;
 	iov = NULL;
 	while ((ch = getopt(argc, argv, "o:vC:")) != -1)
@@ -129,7 +129,7 @@ main(int argc, char **argv)
 		build_iovec(&iov, &iovlen, "cs_disk", cs_disk, (size_t)-1);
 		build_iovec(&iov, &iovlen, "cs_local", cs_local, (size_t)-1);
 	}
-	if (nmount(iov, i, mntflags) < 0)
+	if (nmount(iov, iovlen, mntflags) < 0)
 		err(1, "%s", dev);
 	exit(0);
 }